ab77eb2382297b3afb55fd18076d2561be979844
Jack Cole <123jack.cole@gmail.com>
2020-01-10 06:03:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,7 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto].merge! = {
+  programmer_hash[:yukihiro_matsumoto].merge! {
     :known_for => "Ruby",
     :languages => ["LISP", "C"] 
   }
