d9319089f89324bce22c42637c35bd8f5a5f9a37
Jack Cole <123jack.cole@gmail.com>
2020-01-10 06:28:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -138,7 +138,7 @@ def adding_to_dennis
         }
      }
 
-  programmer_hash[:dennis_ritchie][:languages].merge("Assembly")
+  programmer_hash[:dennis_ritchie][:languages]
   
   programmer_hash
 
